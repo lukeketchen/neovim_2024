8d1b3017f950482eb6f630b1953c09f3c9fe0e4c
Luke Ketchen <luke.ketchen@gmail.com>
2024-04-25 21:34:28 +1000

    show hidden dot files i neo tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -33,6 +33,18 @@ return {
           ['<leader>ee'] = 'close_window',
         },
       },
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          -- '.git',
+          -- '.DS_Store',
+          -- 'thumbs.db',
+        },
+        never_show = {},
+      },
     },
   },
 }
